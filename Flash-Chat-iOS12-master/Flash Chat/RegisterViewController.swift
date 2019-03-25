0938d95bda539e2436e014523e15ae465cb0fabf
shiMac <beau.shirdavani@bellevuecollege.edu>
2019-03-25 16:50:43 -0700

    Firebase Authentication & Registration
    new users can register, and segue to chat screen

--- Flash-Chat-iOS12-master/Flash Chat/RegisterViewController.swift
+++ Flash-Chat-iOS12-master/Flash Chat/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 class RegisterViewController: UIViewController {
@@ -29,14 +30,16 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
-        
         //TODO: Set up a new user on our Firbase database
-        
-        
-
-        
-        
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print(error)
+            } else {
+                // success
+                print("registration successful!")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
     } 
     
     

42f339e0fbf8386006f376e794c485ec3b31464f
shiMac <beau.shirdavani@bellevuecollege.edu>
2019-03-28 19:16:08 -0700

    UX progress spinner
    progress animation for login and register activities, which can be slow.

--- Flash-Chat-iOS12-master/Flash Chat/LogInViewController.swift
+++ Flash-Chat-iOS12-master/Flash Chat/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import Firebase
-
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -26,12 +26,16 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+        // animation progress spin
+        SVProgressHUD.show()
+        
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print(error!)
             } else {
                 print("login successful")
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
